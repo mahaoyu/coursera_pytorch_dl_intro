876c9416ef19a3e90d2fd70ff190d3fd83c54a62
Mat <matthias.wang@icloud.com>
2024-11-02 09:47:27 +0000

    Tidy up imports

--- loss_surface.py
+++ loss_surface.py
@@ -1,8 +1,8 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import torch
-
 from typing import List
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
 
 
 class PlotErrorSurfaces:
